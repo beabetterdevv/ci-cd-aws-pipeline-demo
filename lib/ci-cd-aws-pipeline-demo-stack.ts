2173b4374f7cf56459274f32185c2c8e8be58625
beabetterdev <daniel@beabetterdev.com>
2022-01-29 11:47:36 -0500

    initial commit

---
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+
+export class CiCdAwsPipelineDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'), //Remember to change this later
+        commands: ['npm ci', 
+                   'npm run build', 
+                   'npx cdk synth']
+      })
+    });
+  }
+}
