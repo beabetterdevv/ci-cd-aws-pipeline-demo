b2b2497eaceee69bccea906743ce42f288708765
beabetterdevv <daniel@beabetterdev.com>
2022-01-30 17:36:41 -0500

    added

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,16 +1,21 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+//import { MyPipelineAppStage } from './stage';
 
-export class CiCdAwsPipelineDemoStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CiCdAwsPipelineDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CiCdAwsPipelineDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('beabetterdevv/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
+        commands: ['npm ci', 
+                   'npm run build', 
+                   'npx cdk synth']
+      }),
+    });
   }
 }
