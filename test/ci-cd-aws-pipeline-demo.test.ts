5dd94d24f991c6cc0e45c4c39029ec813f494b0a
beabetterdevv <daniel@beabetterdev.com>
2022-01-30 17:33:40 -0500

    initial commit

---
+++ test/ci-cd-aws-pipeline-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CiCdAwsPipelineDemo from '../lib/ci-cd-aws-pipeline-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ci-cd-aws-pipeline-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CiCdAwsPipelineDemo.CiCdAwsPipelineDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
