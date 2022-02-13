4f0da903c304802b6ca4044dd9e4854577a59501
beabetterdevv <daniel@beabetterdev.com>
2022-02-12 22:05:06 -0500

    Testing adding test shellstep

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 import { MyPipelineAppStage } from './stage';
 
@@ -15,14 +15,17 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
         commands: ['npm ci', 
                    'npm run build', 
                    'npx cdk synth']
-      }),
+      })
     });
+
 
 
     const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
       env: { account: "755314965794", region: "us-east-1" }
     }));
 
+
+    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm test'] }));
     testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
 
     const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
--- test/ci-cd-aws-pipeline-demo.test.ts
+++ test/ci-cd-aws-pipeline-demo.test.ts
@@ -4,6 +4,7 @@
 
 import { handler } from "../lib/lambda/handler";
 
+//https://docs.aws.amazon.com/codebuild/latest/userguide/test-reporting.html
 test('foo', async () => {
     const result = await handler("", "");
     expect(result.statusCode).toEqual(300);
