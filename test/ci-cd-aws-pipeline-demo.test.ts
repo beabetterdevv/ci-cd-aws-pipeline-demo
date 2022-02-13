6bdc375026d5e4706d33bddd9edc6ce59aadf82d
beabetterdevv <daniel@beabetterdev.com>
2022-02-12 21:35:05 -0500

    Adding a failing Unit Test

--- test/ci-cd-aws-pipeline-demo.test.ts
+++ test/ci-cd-aws-pipeline-demo.test.ts
@@ -2,16 +2,9 @@
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as CiCdAwsPipelineDemo from '../lib/ci-cd-aws-pipeline-demo-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/ci-cd-aws-pipeline-demo-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CiCdAwsPipelineDemo.CiCdAwsPipelineDemoStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+import { handler } from "../lib/lambda/handler";
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('foo', async () => {
+    const result = await handler("", "");
+    expect(result.statusCode).toEqual(300);
 });
